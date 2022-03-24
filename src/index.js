41378980f948f3e1cd185a0e7f59f890698595e9
mory2547 <yeop2547@naver.com>
2022-03-24 15:35:06 +0900

    mac

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import Serve from "./Serve";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 ReactDOM.render(
   <App />,
